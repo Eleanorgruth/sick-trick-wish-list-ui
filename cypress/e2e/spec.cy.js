356c4f87727ea988562354e0c7a0c4caa76229b3
Eleanor Ruth <ellie.ruth@me.com>
2023-01-02 15:07:39 -0700

    Add testing for user flow

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('Home page', () => {
     cy.intercept("http://localhost:3001/api/v1/tricks", {
       method: 'GET',
       fixture: '../fixtures/example.json'
-    } )
+    })
     cy.visit('http://localhost:3000');
   });
   it('Should have a title', () => {
@@ -12,7 +12,7 @@ describe('Home page', () => {
   })
   it('Should have a form', () => {
     cy.get('form')
-     .should("exist")
+      .should("exist")
   })
   it('Should have a trick card', () => {
     cy.get('.card')
@@ -23,8 +23,46 @@ describe('Home page', () => {
     cy.get('a')
       .should("have.attr", "href", "https://www.youtube.com/watch?v=9N9swrZU1HA")
   })
-  it('Should have a form', () => {
-    cy.get('form')
-     .should("exist")
+  it('Should allow user to select stance', () => {
+    cy.get('#stance')
+      .select("regular")
+      .should('have.value', "regular")
+  })
+  it('Should allow user type name of trick', () => {
+    cy.get('[placeholder="Name of Trick"]')
+      .type("Super Cool Trick")
+      .should('have.value', "Super Cool Trick")
+  })
+  it('Should allow user to select obstacle', () => {
+    cy.get('#obstacle')
+      .select("rail")
+      .should('have.value', "rail")
+  })
+  it('Should allow user type a link to a tutorial', () => {
+    cy.get('[placeholder="Link to Tutorial"]')
+      .type("www.youtube.com")
+      .should('have.value', "www.youtube.com")
+  })
+  it('Should add a new trick when the send it button is clicked ', () => {
+    cy.get('#stance')
+      .select("regular")
+      .should('have.value', "regular")
+    cy.get('[placeholder="Name of Trick"]')
+      .type("Super Cool Trick")
+      .should('have.value', "Super Cool Trick")
+    cy.get('#obstacle')
+      .select("rail")
+      .should('have.value', "rail")
+    cy.get('[placeholder="Link to Tutorial"]')
+      .type("www.youtube.com")
+      .should('have.value', "www.youtube.com")
+    cy.get('button').click()
+    cy.get('.card-container > :nth-child(2)')
+    cy.get('.card-container > :nth-child(2) > :nth-child(1)')
+      .contains('regular Super Cool Trick')
+    cy.get('.card-container > :nth-child(2) > :nth-child(2)')
+      .contains('Obstacle: rail')
+    cy.get(':nth-child(2) > a')
+      .should("have.attr", "href", "www.youtube.com")
   })
 })
