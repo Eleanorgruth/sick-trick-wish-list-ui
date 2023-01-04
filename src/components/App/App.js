1925baff71f64f5c83bed835176303c8de451522
Eleanor Ruth <ellie.ruth@me.com>
2023-01-04 08:27:14 -0700

    Make function for delete trick

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import './App.css';
-import getData from "../../apiCalls"
+import { getData, postData, deleteData } from "../../apiCalls"
 import CardContainer from '../CardContainer/CardContainer';
 import Form from '../Form/Form'
 
@@ -16,14 +16,19 @@ class App extends Component {
       .then(data=>this.setState({tricks: data}))
   }
   addTrick = (newTrick) => {
-    this.setState({ tricks: [...this.state.tricks, newTrick] })
+    postData(newTrick)
+      .then(data=> this.setState({ tricks: [...this.state.tricks, data] }))
+  }
+  deleteTrick = (id) => {
+    deleteData(id)
+      .then(data=>this.setState({tricks: data}))
   }
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
         <Form addTrick={this.addTrick}/>
-        <CardContainer tricks={this.state.tricks}/>
+        <CardContainer tricks={this.state.tricks} deleteTrick={this.deleteTrick}/>
       </div>
     );
   }
