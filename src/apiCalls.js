9af33ace3e23504f4348f4ecb2c73fc8ac9dcf2c
Eleanor Ruth <ellie.ruth@me.com>
2023-01-04 08:26:25 -0700

    Add Post and Delete function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,4 +3,22 @@ const getData = () => {
     .then(res => res.json())
 }
 
-export default getData  
+const postData = (data) => {
+  return fetch("http://localhost:3001/api/v1/tricks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+}
+
+const deleteData = (id) => {
+  return fetch(`http://localhost:3001/api/v1/tricks/${id}`, {
+    method: "DELETE"
+  })
+  .then(res => res.json())
+}
+
+export { postData, getData, deleteData }  
